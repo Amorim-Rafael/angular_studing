e541ffa8cd295b136ceb1966a0f0274d551766ec
rafael.amorim <rafael.amorim@fortaleza.ce.gov.br>
2017-08-28 11:26:10 -0300

    aula 11 event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,10 +10,23 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com/400/200/nightlife/';
+  valorAtual: string = '';
+  valorSalvo = '';
+  isMouseOver: boolean = false;
 
   getValor(){ return 1; }
 
   getCurtirCurso(){ return true; }
+
+  botaoClicado(){ alert('clicado'); }
+
+  onKeyUp(evento: KeyboardEvent){ 
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){ this.valorSalvo = valor; }
+
+  onMouseOverOut(){ this.isMouseOver = !this.isMouseOver; }
 
   constructor() { }
 
