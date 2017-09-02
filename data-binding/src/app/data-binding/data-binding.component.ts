f89ba04585fceb6e2582886b2a3ae0edf54be023
amorim-rafael <rafaelbrsa@gmail.com>
2017-09-02 12:48:30 -0300

    aula 11 event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,10 +10,20 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com/400/200/nightlife/';
+  valorAtual: string = '';
+  valorSalvo: string = '';
 
   getValor(){ return 1; }
 
   getCurtirCurso(){ return true; }
+
+  botaoClicado() { alert('Botão clicado') }
+
+  onKeyUp(evento: KeyboardEvent){ 
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor) { this.valorSalvo = valor; }
 
   constructor() { }
 
