745b095e093a15c03df0e3a511c23b91c0858d77
amorim-rafael <rafaelbrsa@gmail.com>
2017-09-02 13:03:04 -0300

    merge

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -11,34 +11,22 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com/400/200/nightlife/';
   valorAtual: string = '';
-<<<<<<< HEAD
-  valorSalvo: string = '';
-=======
   valorSalvo = '';
   isMouseOver: boolean = false;
->>>>>>> e541ffa8cd295b136ceb1966a0f0274d551766ec
 
   getValor(){ return 1; }
 
   getCurtirCurso(){ return true; }
 
-<<<<<<< HEAD
-  botaoClicado() { alert('Botão clicado') }
-=======
   botaoClicado(){ alert('clicado'); }
->>>>>>> e541ffa8cd295b136ceb1966a0f0274d551766ec
 
   onKeyUp(evento: KeyboardEvent){ 
     this.valorAtual = (<HTMLInputElement>evento.target).value;
   }
 
-<<<<<<< HEAD
-  salvarValor(valor) { this.valorSalvo = valor; }
-=======
   salvarValor(valor){ this.valorSalvo = valor; }
 
   onMouseOverOut(){ this.isMouseOver = !this.isMouseOver; }
->>>>>>> e541ffa8cd295b136ceb1966a0f0274d551766ec
 
   constructor() { }
 
